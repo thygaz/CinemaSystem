68a6e57dd6cb61fea4bda6c2a8c9bb13d42b9d14
thygaz <thiago.cppj@gmail.com>
2025-09-18 18:24:40 -0300

    Modelo Básico

---
+++ src/Payment/PaymentModel.java
@@ -0,0 +1,9 @@
+package payment;
+
+public class PaymentModel {
+    public String cardNumber;
+    public String cardHolder;
+    public String expirationDate;
+    public String cvv;
+    public double amount;
+}
