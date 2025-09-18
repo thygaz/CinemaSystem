29a9f4d9f671122e25712ae12bab8e0bd2d3c7cb
Yuri Cruz Torquato <yurictorquato@gmail.com>
2025-09-18 11:59:57 -0300

    Algumas funcionalidades feitas na branch 'sessao'

---
+++ src/Sessao.java
@@ -0,0 +1,41 @@
+import java.time.LocalDateTime;
+
+public class Sessao {
+
+    private long id;
+    private Filme filme;
+    private LocalDateTime horaSessao;
+    private Assento assentos;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Filme getFilme() {
+        return filme;
+    }
+
+    public void setFilme(Filme filme) {
+        this.filme = filme;
+    }
+
+    public LocalDateTime getHoraSessao() {
+        return horaSessao;
+    }
+
+    public void setHoraSessao(LocalDateTime horaSessao) {
+        this.horaSessao = horaSessao;
+    }
+
+//    public int getAssentos() {
+//        return assentos;
+//    }
+//
+//    public void setAssentos(int assentos) {
+//        this.assentos = assentos;
+//    }
+}
