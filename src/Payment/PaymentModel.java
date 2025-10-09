791372fe8b5a0966a0e4d7a13d28f40f99026cc2
mauriLeal <mauricio12leal@gmail.com>
2025-10-09 20:50:32 -0300

    Projeto zerado em Spring

--- src/Payment/PaymentModel.java
+++
@@ -1,9 +0,0 @@
-package payment;
-
-public class PaymentModel {
-    public String cardNumber;
-    public String cardHolder;
-    public String expirationDate;
-    public String cvv;
-    public double amount;
-}
