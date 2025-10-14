86ce12d00c76349d54ab4cae7cb18475b67a6548
thygaz <thiago.cppj@gmail.com>
2025-10-14 15:24:38 -0300

    feat: Adiciona estrutura para funcionalida de compra e pagamento

---
+++ CinemaSystem/src/main/java/br/com/cinema/cinemasystem/repository/PurchaseRepository.java
@@ -0,0 +1,24 @@
+package com.cinemasystemspring.repository;
+
+import com.cinemasystemspring.model.Purchase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Set;
+
+
+@Repository
+public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+
+    @Query("SELECT s.id FROM Purchase p JOIN p.seats s WHERE p.movieSession.id = :sessionId AND s.id IN :seatIds")
+    Set<Long> findOccupiedIds(
+            @Param("sessionId") Long sessionId,
+            @Param("seatIds") Set<Long> seatIds
+    );
+
+    List<Purchase> findByUserId(Long userId);
+}
