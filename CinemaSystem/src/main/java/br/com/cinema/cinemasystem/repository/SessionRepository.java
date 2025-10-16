26185c765882ed2b100686cc7f85f43f74a35f98
Yuri <yurictorquato@gmail.com>
2025-10-15 23:37:50 -0300

    feat: cria as interfaces da pasta 'repository'

---
+++ CinemaSystem/src/main/java/br/com/cinema/cinemasystem/repository/SessionRepository.java
@@ -0,0 +1,12 @@
+package br.com.cinema.cinemasystem.repository;
+
+import br.com.cinema.cinemasystem.model.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, UUID> {
+
+}
