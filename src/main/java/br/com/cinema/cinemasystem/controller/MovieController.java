ddd67e94d8f36fa53ed83efe31fd19e5c785d64f
mauriLeal <mauricio12leal@gmail.com>
2025-10-17 09:42:40 -0300

    feat(docker, movie)adiciona CRUD de Filmes e contaneiriza o projeto com docker (evitar conflitos de configurações), testaremos na próxima reunião

---
+++ src/main/java/br/com/cinema/cinemasystem/controller/MovieController.java
@@ -0,0 +1,44 @@
+package br.com.cinema.cinemasystem.controller;
+
+
+import br.com.cinema.cinemasystem.model.Movie;
+import br.com.cinema.cinemasystem.service.MovieService;
+import br.com.cinema.cinemasystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @PostMapping
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie){
+        Movie newMovie = movieService.createMovie(movie);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(movie);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> findMovieById(@PathVariable Long id){
+        Movie movie = movieService.findMovieById(id);
+        return ResponseEntity.ok(movie);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movieDetails){
+        Movie updateMovie = movieService.updateMovie(id, movieDetails);
+
+        return ResponseEntity.ok(updateMovie);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id){
+        movieService.deleteMovie(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/br/com/cinema/cinemasystem/service/UserService.java
+++ src/main/java/br/com/cinema/cinemasystem/service/UserService.java
@@ -43,7 +43,6 @@ public class UserService {
 
     public void deleteUser(Long id){
         User user = userRepository.findUserById(id);
-
         userRepository.delete(user);
     }
 }
