026a069a91835b70318132b6ba6ecefd6bf9ec18
mauriLeal <mauricio12leal@gmail.com>
2025-10-25 21:03:19 -0300

    feat(repository): adiciona método findByEmail retornando Optional<User> ao UserRepository

--- src/main/java/br/com/cinema/cinemasystem/repository/UserRepository.java
+++ src/main/java/br/com/cinema/cinemasystem/repository/UserRepository.java
@@ -4,11 +4,13 @@ import br.com.cinema.cinemasystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findUserById(Long id);
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
 
