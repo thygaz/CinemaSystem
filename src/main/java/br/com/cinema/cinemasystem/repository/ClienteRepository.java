8ea6a338de2100523b0ce0be4911f9f124bd8b09
teste <12724210332@ulife.com.br>
2025-10-30 20:48:03 -0300

    feat(cliente): funcionalidade para acessar, salvar, atualizar e buscar dados de clientes

---
+++ src/main/java/br/com/cinema/cinemasystem/repository/ClienteRepository.java
@@ -0,0 +1,12 @@
+package repository;
+
+import br.com.cinema.cinemasystem.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByNome(String nome);
+}
