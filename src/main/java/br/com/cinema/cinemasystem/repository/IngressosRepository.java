e56952af0238a270c97e04bea40a363755b89e7e
AquinoSantos <aquinisantiago@gmail.com>
2025-11-13 17:49:21 -0300

    chore: Atualização do package
    Atualização do package

--- src/main/java/br/com/cinema/cinemasystem/repository/IngressosRepository.java
+++ src/main/java/br/com/cinema/cinemasystem/repository/IngressosRepository.java
@@ -1,8 +1,9 @@
-package repository;
+package br.com.cinema.cinemasystem.repository;
 
-import br.com.cinema.cinemasystem.model.Ingressos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import br.com.cinema.cinemasystem.model.Ingressos;
 
 @Repository
 public interface IngressosRepository extends JpaRepository<Ingressos, String> {
