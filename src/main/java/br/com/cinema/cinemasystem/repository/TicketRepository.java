330c568f79b341e966b699fba8479751c2d167c4
AquinoSantos <aquinisantiago@gmail.com>
2025-12-03 18:12:09 -0300

    Update: atualização

--- src/main/java/br/com/cinema/cinemasystem/repository/TicketRepository.java
+++ src/main/java/br/com/cinema/cinemasystem/repository/TicketRepository.java
@@ -1,13 +1,12 @@
 package br.com.cinema.cinemasystem.repository;
 
-import java.util.List;
-
+import br.com.cinema.cinemasystem.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.cinema.cinemasystem.model.Ticket;
+import java.util.List;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
-    List<Ticket> findByClienteId(Long clienteId);
+    List<Ticket> findByUserId(Long userId);
 }
